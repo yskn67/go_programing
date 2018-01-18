9461a8db9018f950a43c9d0d599220d6610f72ee
yskn67 <yskn1967@gmail.com>
2018-01-19 00:26:00 +0900

    add chapter5(wip)

--- chapter5/tmpl_iter_engine.go
+++ chapter5/tmpl_iter_engine.go
@@ -11,10 +11,17 @@ func process(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, daysOfWeek)
 }
 
+func noProcess(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("tmpl_iter.html")
+	nothing := []string{}
+	t.Execute(w, nothing)
+}
+
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
 	http.HandleFunc("/process", process)
+	http.HandleFunc("/noprocess", noProcess)
 	server.ListenAndServe()
 }
